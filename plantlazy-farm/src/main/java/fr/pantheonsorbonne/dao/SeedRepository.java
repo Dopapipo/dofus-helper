a87c16c3849d5e05913b5e7f4ac72fb2485325a9
Lucian <lucian@jducraft.com>
2025-01-18 15:39:20 +0100

    fix rebase

--- plantlazy-farm/src/main/java/fr/pantheonsorbonne/dao/SeedRepository.java
+++ plantlazy-farm/src/main/java/fr/pantheonsorbonne/dao/SeedRepository.java
@@ -2,7 +2,7 @@ package fr.pantheonsorbonne.dao;
 
 
 import fr.pantheonsorbonne.entity.SeedEntity;
-import java.util.UUID;
-import org.springframework.data.repository.CrudRepository;
 
-public interface SeedRepository extends CrudRepository<SeedEntity, UUID> {}
+public interface SeedRepository {
+        void save(SeedEntity seed);
+}
