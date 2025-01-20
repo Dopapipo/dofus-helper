de993929500850a907f552ef89da3f586ec9f111
purplegrn <amoutard368@gmail.com>
2025-01-20 14:52:15 +0100

    chore: fix init stock method

--- plantlazy-dashboard/src/main/java/fr/pantheonsorbonne/service/InitService.java
+++ plantlazy-dashboard/src/main/java/fr/pantheonsorbonne/service/InitService.java
@@ -1,7 +1,9 @@
 package fr.pantheonsorbonne.service;
 
+import io.quarkus.runtime.StartupEvent;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
@@ -24,16 +26,21 @@ public class InitService {
     @ConfigProperty(name = "money.amount")
     Double moneyAmount;
 
+    public void onStart(@Observes StartupEvent event) {
+        System.out.println("🔄 Initialisation via StartupEvent...");
+    }
+
     @PostConstruct
     public void initialize() {
         try {
+            System.out.println("🔄 Initialisation de la simulation...");
 
             // Appel à ms-plantlazy-stock pour initialiser MONEY
             InitMoneyDTO moneyDTO = new InitMoneyDTO(moneyAmount); // Montant initial par défaut
             stockService.initializeMoney(moneyDTO);
 
             // Appel à ms-plantlazy-server-ticks pour démarrer les ticks
-            serverTickService.startTicks();
+            // serverTickService.startTicks();
 
         } catch (Exception e) {
             System.err.println("Erreur lors de l'initialisation : " + e.getMessage());
