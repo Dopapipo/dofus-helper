50e35441d938ff2aa3f840910ec571110974f5fb
purplegrn <amoutard368@gmail.com>
2025-01-21 22:37:30 +0100

    fix + revert changes

--- plantlazy-dashboard/src/main/java/fr/pantheonsorbonne/service/InitService.java
+++ plantlazy-dashboard/src/main/java/fr/pantheonsorbonne/service/InitService.java
@@ -1,39 +1,25 @@
 package fr.pantheonsorbonne.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.pantheonsorbonne.dto.InitMoneyDTO;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import jakarta.jms.JMSContext;
-import org.apache.camel.CamelContext;
-import org.apache.camel.ProducerTemplate;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 @ApplicationScoped
 public class InitService {
-    @Inject
-    JMSContext context;
 
     @Inject
-    @ConfigProperty(name = "init.endpoint")
-    String initEndpoint;
-
+    @RestClient
+    StockService stockService;
 
     @Inject
     @RestClient
     ServerTickService serverTickService;
 
-    public void initializeSimulation(InitMoneyDTO initMoneyDTO) {
-        this.initMoney(initMoneyDTO);
-        serverTickService.startTicks();
-    }
+    public void initializeSimulation(double money) {
+        InitMoneyDTO initMoneyDTO = new InitMoneyDTO(money);
+        stockService.initializeMoney(initMoneyDTO);
 
-    private void initMoney(InitMoneyDTO initMoneyDTO) {
-        try {
-            context.createProducer().send(context.createQueue(initEndpoint), context.createTextMessage(new ObjectMapper().writeValueAsString(initMoneyDTO)));
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to serialize InitMoneyDTO", e);
-        }
+        serverTickService.startTicks();
     }
 }
